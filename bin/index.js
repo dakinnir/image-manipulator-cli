02e3c5dac13125bee9732de761180f56a9e51ed2
dakinnir <dakinnir@gmail.com>
2023-08-04 02:29:22 -0400

    new changes

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
-import yargs from "yargs";
-import { hideBin } from 'yargs/helpers'
+#! /usr/bin/env node
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 import chalk from "chalk";
 
 /**
@@ -8,15 +9,25 @@ import chalk from "chalk";
  * @returns {void}
  */
 
+const options = yargs(hideBin(process.argv))
+.usage("Usage: $0 [options]")
+.option("m", {alias:"url", describe: "The path url to the image", type: "string", demandOption: true })
+.option("f", {alias:"flip", describe: "Flip the image", type: "boolean", demandOption: true })
+.help(true)
+.argv;
+
+
+
 function editImage() {
-    const argv = yargs(hideBin(process.argv)).argv
-    const args = argv._;
-    if (args.length < 1) {
-        console.log(chalk.red("Please provide an image path"))
-        return;
-    }
-    console.log(args)
-    console.log(chalk.green("Image path was successfully edited"))
+  const argv = yargs(hideBin(process.argv)).argv;
+  console.log(argv);
+  const args = argv._;
+  if (args.length < 1) {
+    console.log(chalk.red("Please provide an image path"));
+    return;
+  }
+  console.log(args);
+  console.log(chalk.green("Image path was successfully edited"));
 }
 
 editImage();
